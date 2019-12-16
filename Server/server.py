715625974dae7190be3e2f076ee44f8ac17e82d1
minhyoung0724 <minhyoung0724@gmail.com>
2019-12-16 15:23:31 +0900

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 
-
 app = Flask(__name__)
+
+dest = ''
+is_arrived = False
 
 @app.route('/')
 def hello_world():
@@ -9,11 +11,21 @@ def hello_world():
 
 @app.route('/pickup')
 def pickup():
-    return 'PickUp!'
+    global dest
+    global is_arrived
+
+    dest = request.args.get('dest', 0)
+    is_arrived = False
+
+    return 'PickUp! Dest: ' + dest
 
 @app.route('/arrive')
 def arrive():
     global dest
+    global is_arrived
+
+    dest = dest
+    is_arrived = True
 
     cost = request.args.get('cost', 0)
     json_cost = {'cost' : cost}
@@ -23,7 +35,20 @@ def arrive():
 @app.route('/pay')
 def pay():
     global dest
+    global is_arrived
+
     dest = ''
+    is_arrived = False
+
+    return 'Paid!'
+
+@app.route('/getdata')
+def get():
+    global dest
+    global is_arrived
+
+    data = {'dest': dest, 'is_arrive': is_arrived}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
